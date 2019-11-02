5488becaf658afec322cc06b918920a3629a821e
GALLAIS-POU Raphaël <raphael.gallais.pou@gmail.com>
2019-11-02 02:43:19 +0100

    Add check neighbours

--- src/cell.rs
+++ src/cell.rs
@@ -1,9 +1,15 @@
-
+#[derive(Clone, Copy, Debug)]
 pub struct Cell {
     alive : bool,
     x : u32,
     y : u32,
 }
+
+// impl Clone for Cell {
+//     pub fn clone (&self) -> Cell {
+//         *self
+//     }
+// }
 
 impl Cell {
     pub fn new(alive : bool, x : u32, y : u32) -> Cell {
@@ -19,21 +25,20 @@ impl Cell {
     }
 
     // Getter for cell ?
+    pub fn get_x(&self) -> u32 {
+        self.x
+    }
 
-    // Get all neighbours and then process
-    pub fn process_neighbours(&mut self) -> bool{
-        for i in self.x-1..self.x+1 {
-            for j in self.y-1..self.y+1 {
-                // self.
-            }
-        }
+    pub fn get_y(&self) -> u32 {
+        self.y
+    }
 
-        self.alive = self.check_neighbours(0);
-        self.is_alive()
+    pub fn process(&mut self, neighbours : u16) {
+        self.alive = self.process_neighbours(neighbours);
     }
 
     // Process if alive according to neighbours
-    fn check_neighbours(&self, neighbours : u16) -> bool {
+    pub fn process_neighbours(&self, neighbours : u16) -> bool {
         if self.alive && neighbours < 2 {
             // Underpopulation
             false
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,62 @@
 mod cell;
 
-use std::env;
+use std::{env, time, thread};
+use rand::Rng;
 use termion;
 
 const CELL : &str = "x";
 const HOLE : &str = " ";
 
 fn main() {
-    println!("Hello, world!");
     // Get cli args
-    // let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
 
     let (height, width) = termion::terminal_size().unwrap();
-    // let grid_size = 
-
     let mut grid = Vec::<cell::Cell>::with_capacity((height * width) as usize);
-
+    let mut rng = rand::thread_rng();
+    let timer = time::Duration::from_millis(250);
+    
+    // Grid creation
     for i in 0..height as u32 {
         for j in 0..width as u32 {
-            grid.push(cell::Cell::new(true, i, j));
+            grid.push(cell::Cell::new(rng.gen::<bool>(), i, j));
         }
     }
+
+    // Debug
+    // println!("{:?}", grid);
+    assert_eq!(grid.len() as u16, height * width);
+
+    loop {
+        for mut cell in grid.clone() {
+            cell.process(check_neighbours(grid.clone(), &cell));
+            // if cell.process_neighbours() {
+            if cell.is_alive() {
+                print!("{}", CELL);
+            } else {
+                print!("{}", HOLE);
+            }
+        }
+        print!("{}", termion::clear::All);
+        thread::sleep(timer);
+    }
+}
+
+fn check_neighbours(grid : Vec<cell::Cell>, c : &cell::Cell) -> u16 {
+    let mut neighbours = 0;
+    // TODO Resolve that
+    // Panicked when does "-1" instruction
+    for i in c.get_x()-1 ..c.get_x()+1 {
+        for j in c.get_y()-1 ..c.get_y()+1 {
+            // Find cell corresponding to coord
+            // and do stuff
+            let cell = grid.iter().find(|cell| cell.get_x() == i && cell.get_y() == j).unwrap();
+            println!("{:?}", cell);
+            if cell.is_alive() {
+                neighbours += 1;
+            }
+        }
+    }
+    neighbours
 }
